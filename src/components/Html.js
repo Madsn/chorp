4b39f57ffaf77bd42c8dd170f2bdd64ee35af8e8
Konstantin Tarkus <hello@tarkus.me>
2016-09-27 09:56:21 +0300

    A bare minimum Redux integration

--- src/components/Html.js
+++ src/components/Html.js
@@ -8,9 +8,10 @@
  */
 
 import React, { PropTypes } from 'react';
+import serialize from 'serialize-javascript';
 import { analytics } from '../config';
 
-function Html({ title, description, style, script, children }) {
+function Html({ title, description, style, script, state, children }) {
   return (
     <html className="no-js" lang="en">
       <head>
@@ -25,6 +26,11 @@ function Html({ title, description, style, script, children }) {
       </head>
       <body>
         <div id="app" dangerouslySetInnerHTML={{ __html: children }} />
+        {state && <script
+          dangerouslySetInnerHTML={{
+            __html: `window.APP_STATE=${serialize(state, { isJSON: true })}`,
+          }}
+        />}
         {script && <script src={script} />}
         {analytics.google.trackingId &&
           <script
@@ -46,6 +52,7 @@ Html.propTypes = {
   description: PropTypes.string.isRequired,
   style: PropTypes.string,
   script: PropTypes.string,
+  state: PropTypes.object,
   children: PropTypes.string,
 };
 
