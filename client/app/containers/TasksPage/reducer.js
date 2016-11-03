3274ec89b627fb0eec81bb557f0da2b660c98492
Madsn <m@madsn.net>
2016-11-03 21:27:49 +0100

    WIP - Task component with dummy data

--- client/app/containers/TasksPage/reducer.js
+++ client/app/containers/TasksPage/reducer.js
@@ -16,34 +16,14 @@ const initialState = fromJS({
   tasks: []
 });
 
-function addTask(state, newTask) {
-  return fromJS({
-    tasks: state.get('tasks').push(newTask)
-  })
-}
-
-function removeTask(state, key) {
-  console.log('removing', key);
-  var index = findIndex(state.get('tasks'), ['key', key]);
-  console.log('index', index);
-  if (index >= 0) {
-    return fromJS({
-      tasks: state.get('tasks').splice(index, 1)
-    })
-  } else {
-    return state;
-  }
-}
-
 function tasksPageReducer(state = initialState, action) {
-  console.log('tasksPageReducer got action of type ', action.type);
   switch (action.type) {
     case ADD_TASK_ACTION:
-      return addTask(state, action.task);
+      return state
+        .setIn(['tasks'], state.get('tasks').push(action.task));
     case REMOVE_TASK_ACTION:
-      console.log(action);
-      console.log(action.key);
-      return removeTask(state, action.key);
+      return state
+        .setIn(['tasks'], state.get('tasks').filter(function(obj){ return obj.key != action.key}));
     case DEFAULT_ACTION:
       return state;
     default:
