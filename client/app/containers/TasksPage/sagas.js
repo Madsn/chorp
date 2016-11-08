9b3dc71b3434027fa6e12e6893ae26eec536cf0c
Madsn <m@madsn.net>
2016-11-08 20:38:44 +0100

    Add rest endpoints for tasks

--- client/app/containers/TasksPage/sagas.js
+++ client/app/containers/TasksPage/sagas.js
@@ -1,11 +1,30 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga';
+import { call, put } from 'redux-saga/effects';
+import {
+  ADD_TASK_ACTION,
+  ADD_TASK_SUCCEEDED,
+  ADD_TASK_FAILED,
+} from './constants';
+import { addTask } from '../../api/Tasks';
 
-// Individual exports for testing
-export function* defaultSaga() {
-  return;
+function addTaskApiCall(args) {
+  console.log('addTaskApiCall called');
+}
+
+export function* addTask(action) {
+  try {
+    const task = yield call(addTaskApiCall, action.payload);
+    yield put({type: ADD_TASK_SUCCEEDED, task: task})
+  } catch (e) {
+    yield put({type: ADD_TASK_FAILED, message: e.message});
+  }
+}
+
+function* addTaskSaga() {
+  yield* takeEvery(ADD_TASK_ACTION, addTask);
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  addTaskSaga
 ];
