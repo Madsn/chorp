a21d53ed6d12a282c35d9f0257c718fbf4a6502e
Madsn <m@madsn.net>
2016-11-12 08:44:17 +0100

    Add redux-saga to fountain-react-es6

--- alternative clients/fountain-react-es6/src/app/store/configureStore.js
+++ alternative clients/fountain-react-es6/src/app/store/configureStore.js
@@ -1,8 +1,17 @@
-import {createStore} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import rootReducer from '../reducers/index';
+import createSagaMiddleware from 'redux-saga';
+
+import mySaga from '../actions/saga';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState, composeEnhancers(
+    applyMiddleware(sagaMiddleware),
+  ));
+  sagaMiddleware.run(mySaga);
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
