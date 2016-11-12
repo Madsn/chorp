fbdadb8cbeeb72c98e633a065fdc75a1f3d9db94
Madsn <m@madsn.net>
2016-11-12 08:45:32 +0100

    add run script for django

--- api_server/chorp_rest_api/test_serializers.py
+++ api_server/chorp_rest_api/test_serializers.py
@@ -3,12 +3,22 @@ from chorp_models.models import Task
 from chorp_rest_api.serializers import TaskSerializer
 
 
+def get_model_fields(model):
+    return model._meta.fields
+
+
+def get_name_from_serializer_field(field):
+    return field.split('.')[2]
+
+
 class SerializersTestCase(TestCase):
 
     def test_task_serializer_contains_all_fields(self):
         serializer = TaskSerializer()
-        for f in Task()._meta.fields:
+        for field in get_model_fields(Task()):
             # chorp_models.Task.id etc.
-            field_name = str(f).split('.')[2]
+            field_name = get_name_from_serializer_field(field)
             if field_name not in ['id', 'created']:
                 self.assertIsNotNone(serializer.fields[field_name])
+
+
