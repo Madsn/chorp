286e1decee4421300cccff5fb4bd8b2855116a23
Madsn <m@madsn.net>
2016-11-15 19:17:32 +0100

    cleanup

--- api_server/scripts/reset_dev_db.bat
+++ api_server/scripts/reset_dev_db.bat
@@ -1,6 +1,6 @@
-del ../chorp_api/migrations/0*.py
+del ../**/migrations/0*.py
 del db.sqlite3
-python manage.py makemigrations
+python manage.py makemigrations chorp_models
 python manage.py migrate
 python manage.py loaddata sampledata.json
 python manage.py loaddata devadmin.json
